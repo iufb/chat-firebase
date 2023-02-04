64b896957c1a188f2da21fb8f9b9cdf6cc6a6fe9
iufb <misakilover228@gmail.com>
2023-02-04 22:42:53 +0600

    add firebase

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA6nTQ6W5pIGfPNNmoQXG0WPn0Vbsf7ZnA",
+  authDomain: "chat-a754f.firebaseapp.com",
+  projectId: "chat-a754f",
+  storageBucket: "chat-a754f.appspot.com",
+  messagingSenderId: "364713763255",
+  appId: "1:364713763255:web:2ea467536afff309c60832",
+};
+
+const app = initializeApp(firebaseConfig);
