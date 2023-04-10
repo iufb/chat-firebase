a997dcb6a18b90e45f0cb8b4e8dd09ecbc0e05b2
iufb <misakilover228@gmail.com>
2023-04-10 12:51:08 +0600

    redirect to home

--- src/layout/PageLayout/PageLayout.tsx
+++ src/layout/PageLayout/PageLayout.tsx
@@ -1,13 +1,18 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { Navbar } from "../../components";
 
 export const PageLayout = (): JSX.Element => {
+  const { pathname } = useLocation();
+  const shouldRedirect = pathname == "/";
   return (
-    <div className="h-screen  overflow-hidden">
-      <Navbar />
-      <div className="container ">
-        <Outlet />
+    <>
+      <div className="h-screen  overflow-hidden">
+        <Navbar />
+        <div className="container ">
+          <Outlet />
+        </div>
       </div>
-    </div>
+      {shouldRedirect && <Navigate replace to="/home" />}
+    </>
   );
 };
